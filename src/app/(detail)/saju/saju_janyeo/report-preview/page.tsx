f68e2868869907fa32507bd84c5daf4d6b4a8ad5
haedam365 <haedam365@gmail.com>
2026-08-01 00:58:07 +0900

    feat(saju_janyeo): 제1장 성향과 기질 파트 위에 saju_janyeo_1_1.jpg 이미지 삽입

--- src/app/(detail)/saju/saju_janyeo/report-preview/page.tsx
+++ src/app/(detail)/saju/saju_janyeo/report-preview/page.tsx
@@ -4575,6 +4575,14 @@ function ReportPreviewInner() {
             </span>{` 버튼을 누르면\n언제든 다시 꺼내볼 수 있소.`}</Quote>
 
           {/* 원국 분석 */}
+          <div className="relative overflow-hidden mt-8" style={{ height: 340 }}>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src="/media/report/saju_janyeo/saju_janyeo_1/saju_janyeo_1_1.jpg" alt="" className="absolute inset-0 w-full h-full object-cover" style={{ objectPosition: "center center" }} />
+            <div className="absolute inset-0" style={{ background: "linear-gradient(to bottom, transparent 50%, rgba(253,248,244,1) 100%)" }} />
+          </div>
+          <div className="flex justify-center" style={{ paddingTop: 40, paddingBottom: 32 }}>
+            <div style={{ width: 1, height: 60, background: "linear-gradient(to bottom, rgba(180,120,100,0.3), rgba(180,120,100,0.6))" }} />
+          </div>
           <section className="pt-6 pb-12">
             <div className="px-6">
               <Heading>아이의 타고난 성향과 기질</Heading>
