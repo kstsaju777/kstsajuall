d2950252374679ad74079ec2a0c9f30617ed40ef
haedam365 <haedam365@gmail.com>
2026-08-01 15:19:09 +0900

    feat(saju_youare): 제6장 아이를 위한 개운법 위 삽화 saju_youare_6_1.jpg 삽입

--- src/app/(detail)/saju/saju_youare/report-preview/page.tsx
+++ src/app/(detail)/saju/saju_youare/report-preview/page.tsx
@@ -5799,6 +5799,14 @@ function ReportPreviewInner() {
                 </section>
 
                 {/* ② 개운법 카드 */}
+                <div className="relative overflow-hidden mt-8" style={{ height: 340 }}>
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img src="/media/report/saju_youare/saju_youare_6/saju_youare_6_1.jpg" alt="" className="absolute inset-0 w-full h-full object-cover" style={{ objectPosition: "center center" }} />
+                  <div className="absolute inset-0" style={{ background: "linear-gradient(to bottom, transparent 50%, rgba(253,248,244,1) 100%)" }} />
+                </div>
+                <div className="flex justify-center" style={{ paddingTop: 40, paddingBottom: 32 }}>
+                  <div style={{ width: 1, height: 60, background: "linear-gradient(to bottom, rgba(180,120,100,0.3), rgba(180,120,100,0.6))" }} />
+                </div>
                 {rd.remedies && rd.remedies.length > 0 && (
                   <section className="px-6 pt-0 pb-4">
                     <Heading>아이를 위한 개운법</Heading>
